707518f872bab88b7cc7482e05f9298bb812d85d
Mahmoud Mosbah <46773011+mosb7@users.noreply.github.com>
2021-10-21 20:10:29 +0200

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE registeration;
+USE registeration;
+CREATE TABLE users(
+    username varchar(254) not null ,
+    email varchar(254) not null,
+    password varchar(254) not null
+    );
